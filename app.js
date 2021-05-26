7add02b087f5cfaea0251c169a75522759c95180
sabri-manai <60587331+sabri-manai@users.noreply.github.com>
2021-05-26 18:36:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
-const app2 = express()
-const app1 = express()
+const app = express()
 const db = require('./config/database')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -9,108 +8,70 @@ const passport = require('passport')
 const passportSetup = require('./config/passport-setup')
 
 
-const handler = serverNum => (req, res) => {
-    console.log(`server ${serverNum}`, req.method, req.url, req.body);
-    res.send(`Hello from server ${serverNum}!`);
-
-  };
-
-app2.set('view engine', 'ejs')
-app1.set('view engine', 'ejs')
 
 
-app2.use(bodyParser.urlencoded({ extended: false }))
-app1.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
 
-app2.use(bodyParser.json())
-app1.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 
 
-app2.use(express.static('public'))
-app1.use(express.static('public'))
+app.use(express.static('public'))
 
-app2.use(express.static('uploads'))
-app1.use(express.static('uploads'))
+app.use(express.static('uploads'))
 
-app2.use(express.static('node_modules'))
-app1.use(express.static('node_modules'))
+app.use(express.static('node_modules'))
 
-
-app2.use(session({
+app.use(session({
     secret: 'lorem ipsum',
     resave: false,
     saveUninitialized: false,
     cookie: {maxAge: 60000 * 15}
 }))
-app1.use(session({
-    secret: 'lorem ipsum',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 60000 * 15}
-}))
-app2.use(flash())
-app1.use(flash())
+app.use(flash())
 
+app.use(passport.initialize())
 
-app2.use(passport.initialize())
-app1.use(passport.initialize())
-
-app2.use(passport.session())
-app1.use(passport.session())
+app.use(passport.session())
 
 
 
-app2.get('*',  handler(2)).post('*', handler(2), (req,res,next)=> {
-   ( res.locals.user = req.user || null)
-    next()
-})
-
-app1.get('*',  handler(1)).post('*', handler(1), (req,res,next)=> {
+app.get('*',  (req,res,next)=> {
     ( res.locals.user = req.user || null)
      next()
  })
 
 
-app1.get('/', handler(1)).post('*', handler(1), (req,res)=> {
+app.get('/', (req,res)=> {
 
    res.redirect('/articles')
     
 })
 
-app2.get('/', handler(2)).post('*', handler(2), (req,res)=> {
 
-    res.redirect('/articles')
-     
- })
 
 
 const articles = require('./routes/article-routes')
-app1.use('/articles', articles)
-app2.use('/articles', articles)
+app.use('/articles', articles)
 
 const suppliers = require('./routes/supplier-routes')
-app1.use('/suppliers', suppliers)
-app2.use('/suppliers', suppliers)
+app.use('/suppliers', suppliers)
 
 const users = require('./routes/user-routes')
-app1.use('/users', users)
-app2.use('/users', users)
+app.use('/users', users)
 
 
 const products = require('./routes/product-routes')
-app1.use('/products', products)
-app2.use('/products', products)
+app.use('/products', products)
 
 
 
-app1.listen(5000, ()=> {
+app.listen(5000, ()=> {
 
     console.log(' app is wokring on port 5000')
 })
 
-app2.listen(5001, ()=> {
-
-    console.log(' app is wokring on port 5001')
-})
 
